6c19897aa5d6180363eca1dbdf4156e5353c85b1
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-08 04:04:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   lowest_name = ""
-  lowest_num = name_hash[name]
+  lowest_num = ""
   name_hash.each do |name, number|
     if lowest_num > number
       lowest_num = number
